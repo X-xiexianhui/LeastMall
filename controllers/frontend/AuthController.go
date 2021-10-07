b7344093ceb734ce854a95ae7db72fd233a5df0e
xiexianhui <xiexianhuicn@outlook.com>
2021-10-07 15:33:01 +0800

    Bugfix

--- controllers/frontend/AuthController.go
+++ controllers/frontend/AuthController.go
@@ -42,6 +42,7 @@ func (c *AuthController) GoLogin() {
 			"msg":     "用户登陆成功",
 		}
 		c.ServeJSON()
+		models.Cookie.Get(c.Ctx, "userinfo", user[0])
 		return
 	} else {
 		c.Data["json"] = map[string]interface{}{
