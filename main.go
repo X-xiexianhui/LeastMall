0925535afce277c4caf010f76a42b677221e82c2
xiexianhui <xiexianhuicn@outlook.com>
2022-05-24 17:49:50 +0800

    "redis config"

--- main.go
+++ main.go
@@ -8,11 +8,14 @@ package main
 
 import (
 	"github.com/gin-gonic/gin"
+	"io"
+	"os"
 )
 
 func main() {
 	//日志着色
 	gin.ForceConsoleColor()
+	gin.DefaultWriter = io.MultiWriter(os.Stdout)
 	//启动gin
 	r := gin.Default()
 	_ = r.Run(":8080")
