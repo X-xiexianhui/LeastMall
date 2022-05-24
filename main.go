d40d66346098d8f1c08194a551775c8d9e52208d
xiexianhui <xiexianhuicn@outlook.com>
2022-05-24 19:44:29 +0800

    "redis config"

--- main.go
+++ main.go
@@ -8,9 +8,25 @@ package main
 
 import (
 	"github.com/gin-gonic/gin"
+	"github.com/spf13/viper"
 	"leastMall_gin/modules"
+	"log"
 )
 
+func init() {
+	cfg := viper.New()
+	cfg.SetConfigName("config")
+	cfg.SetConfigFile("./conf/app.yaml")
+	if err := cfg.ReadInConfig(); err != nil { // 必须 先 读取 `ReadInConfig`
+		log.Panicln(err)
+	}
+	err := cfg.Unmarshal(&modules.Conf)
+	if err != nil {
+		log.Panicln("参数配置失败")
+	}
+	cfg.WatchConfig()
+	log.Println("参数配置成功")
+}
 func main() {
 	modules.InitDataBase()
 	//日志着色
