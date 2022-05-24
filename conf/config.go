4e29a7af1eb77dabbc0d5dd2227954fe6087b0c8
xiexianhui <xiexianhuicn@outlook.com>
2022-05-24 16:41:12 +0800

    "redis config"

--- conf/config.go
+++ conf/config.go
@@ -6,13 +6,22 @@
 */
 package conf
 
-import "github.com/spf13/viper"
+import (
+	"github.com/spf13/viper"
+	"time"
+)
 
 type Mysql struct {
 	User     string `yaml:"user"`
 	Password string `yaml:"password"`
 	Host     string `yaml:"host"`
 	Port     string `yaml:"port"`
+}
+type Cache struct {
+	Host        string        `yaml:"host"`
+	Password    string        `yaml:"password"`
+	DefaultDB   int           `yaml:"defaultDB"`
+	DialTimeout time.Duration `yaml:"dialTimeout"`
 }
 
 var configPath = "./app.yaml"
