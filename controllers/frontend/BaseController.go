92114b646562401ed796704aa6686a6af5bf1d8a
xiexianhui <xiexianhuicn@outlook.com>
2022-05-25 21:23:30 +0800

    "models"

--- controllers/frontend/BaseController.go
+++ controllers/frontend/BaseController.go
@@ -2,69 +2,68 @@ package frontend
 
 import (
 	"fmt"
+	"github.com/gin-gonic/gin"
 	"github.com/jinzhu/gorm"
+	"leastMall_gin/conn"
 	"leastMall_gin/models"
+	"net/http"
 	"net/url"
 	"strings"
 )
 
-type BaseController struct {
-	beego.Controller
-}
-
-func (c *BaseController) BaseInit() {
+func BaseInit(c *gin.Context) {
 	//获取顶部导航
-	topMenu := []models.Menu{}
-	if hasTopMenu := models.CacheDb.Get("topMenu", &topMenu); hasTopMenu == true {
-		c.Data["topMenuList"] = topMenu
+	var topMenu []models.Menu
+	if hasTopMenu := models.CacheDb.Get(c, "topMenu", &topMenu); hasTopMenu == true {
+		c.JSON(http.StatusOK, topMenu)
 	} else {
-		models.DB.Where("status=1 AND position=1").Order("sort desc").Find(&topMenu)
-		c.Data["topMenuList"] = topMenu
-		models.CacheDb.Set("topMenu", topMenu)
+		conn.Db.Where("status=1 AND position=1").Order("sort desc").Find(&topMenu)
+		c.JSON(http.StatusOK, topMenu)
+		models.CacheDb.Set(c, "topMenu", topMenu, 30*24*60*60)
 	}
 
 	//左侧分类（预加载）
-	productCate := []models.ProductCate{}
+	var productCate []models.ProductCate
 
-	if hasProductCate := models.CacheDb.Get("productCate",
+	if hasProductCate := models.CacheDb.Get(c, "productCate",
 		&productCate); hasProductCate == true {
-		c.Data["productCateList"] = productCate
+		c.JSON(http.StatusOK, productCate)
 	} else {
-		models.DB.Preload("ProductCateItem",
+		conn.Db.Preload("ProductCateItem",
 			func(db *gorm.DB) *gorm.DB {
 				return db.Where("product_cate.status=1").
 					Order("product_cate.sort DESC")
 			}).Where("pid=0 AND status=1").Order("sort desc", true).
 			Find(&productCate)
-		c.Data["productCateList"] = productCate
-		models.CacheDb.Set("productCate", productCate)
+		c.JSON(http.StatusOK, productCate)
+		models.CacheDb.Set(c, "productCate", productCate, 30*24*60*60)
 	}
 
 	//获取中间导航的数据
-	middleMenu := []models.Menu{}
-	if hasMiddleMenu := models.CacheDb.Get("middleMenu",
+	var middleMenu []models.Menu
+	if hasMiddleMenu := models.CacheDb.Get(c, "middleMenu",
 		&middleMenu); hasMiddleMenu == true {
-		c.Data["middleMenuList"] = middleMenu
+		c.JSON(http.StatusOK, middleMenu)
 	} else {
-		models.DB.Where("status=1 AND position=2").Order("sort desc").
+		conn.Db.Where("status=1 AND position=2").Order("sort desc").
 			Find(&middleMenu)
 
 		for i := 0; i < len(middleMenu); i++ {
 			//获取关联商品
 			middleMenu[i].Relation = strings.ReplaceAll(middleMenu[i].Relation, "，", ",")
 			relation := strings.Split(middleMenu[i].Relation, ",")
-			product := []models.Product{}
-			models.DB.Where("id in (?)", relation).Limit(6).Order("sort ASC").
+			var product []models.Product
+			conn.Db.Where("id in (?)", relation).Limit(6).Order("sort ASC").
 				Select("id,title,product_img,price").Find(&product)
 			middleMenu[i].ProductItem = product
 		}
-		c.Data["middleMenuList"] = middleMenu
-		models.CacheDb.Set("middleMenu", middleMenu)
+		c.JSON(http.StatusOK, middleMenu)
+		models.CacheDb.Set(c, "middleMenu", middleMenu, 30*24*60*60)
 	}
 
 	//判断用户是否登陆
 	user := &models.User{}
-	models.Cookie.Get(c.Ctx, "userinfo", user)
+	models.Cookie.Get(c, "userinfo", user)
 	if len(user.Phone) == 11 {
 		str := fmt.Sprintf(`<ul>
 			<li class="userinfo">
@@ -81,15 +80,15 @@ func (c *BaseController) BaseInit() {
 
 			</li>
 		</ul> `, user.Phone)
-		c.Data["userinfo"] = str
+		c.JSON(http.StatusOK, str)
 	} else {
 		str := fmt.Sprintf(`<ul>
 			<li><a href="/auth/login" target="_blank">登录</a></li>
 			<li>|</li>
 			<li><a href="/auth/registerStep1" target="_blank" >注册</a></li>
 		</ul>`)
-		c.Data["userinfo"] = str
+		c.JSON(http.StatusOK, str)
 	}
-	urlPath, _ := url.Parse(c.Ctx.Request.URL.String())
-	c.Data["pathname"] = urlPath.Path
+	urlPath, _ := url.Parse(c.Request.URL.String())
+	c.JSON(http.StatusOK, urlPath)
 }
