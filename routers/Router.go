fda5763e222ace5d95cc682424f15b9d2f1ceb91
xiexianhui <xiexianhuicn@outlook.com>
2022-06-02 22:45:50 +0800

    product

--- routers/Router.go
+++ routers/Router.go
@@ -14,14 +14,19 @@ import (
 var Router = gin.Default()
 
 func init() {
+	//轮播图api
 	Router.GET("/banner/get", controllers.GetBanner)
 	Router.POST("/banner/add", controllers.AddBanner)
 	Router.DELETE("/banner/delete", controllers.DeleteBanner)
 	Router.GET("/banner/query", controllers.QueryBanner)
 	Router.POST("/banner/update", controllers.UpdateBanner)
-
+	//商品api
 	Router.GET("/product/get", controllers.GetProducts)
 	Router.POST("/product/add", controllers.AddProduct)
 	Router.DELETE("/product/delete", controllers.DeleteProduct)
 	Router.POST("/product/update", controllers.UpdateProduct)
+	//商品相册api
+	Router.GET("/image/get", controllers.GetImage)
+	Router.POST("/image/add", controllers.AddImages)
+	Router.DELETE("/image/delete", controllers.DeleteImage)
 }
