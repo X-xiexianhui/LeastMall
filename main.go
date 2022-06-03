cb6fc362af970df0b73a9b1a62bf483c4a8074ad
xiexianhui <xiexianhuicn@outlook.com>
2022-06-03 23:24:40 +0800

    pprof

--- main.go
+++ main.go
@@ -9,9 +9,16 @@ package main
 import (
 	"github.com/gin-gonic/gin"
 	"leastMall_gin/routers"
+	"log"
+	"net/http"
+	_ "net/http/pprof"
 )
 
 func main() {
+	go func() {
+		log.Println(http.ListenAndServe(":6060", nil))
+	}()
+
 	//日志着色
 	gin.ForceConsoleColor()
 	//启动gin
