8163ea2ba99f17e21b6f8541cf3b21b58c6a9f7a
xiexianhui <xiexianhuicn@outlook.com>
2022-06-03 11:26:43 +0800

    search

--- conn/ESConect.go
+++ conn/ESConect.go
@@ -18,7 +18,7 @@ func init() {
 	cfg := Conf.ES
 	EsClient, err = elastic.NewClient(elastic.SetURL("http://"+cfg.Host+":"+cfg.Port), elastic.SetSniff(false))
 	if err != nil {
-		log.Println(err)
+		log.Panicln(err)
 	}
 	log.Println("elasticsearch connect success")
 }
